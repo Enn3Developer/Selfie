9ec8e50834887accf0ac3f5ce351eed4156ad025
Enn3Developer <arturpeshko39@gmail.com>
2025-05-06 19:07:32 +0200

    cleanup tokens every 5 minutes

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,17 @@
 import express = require("express");
 import cors = require("cors");
 import {setupRoutes} from "./routes/routes";
+import {cleanUpTokens} from "./token";
 
 const app = express();
+// 5 minutes
+const CLEANUP_TIME = 5 * 60 * 1000;
 
 app.use(cors());
 
 setupRoutes(app);
+
+setInterval(cleanUpTokens, CLEANUP_TIME);
 
 app.listen(8080, () => {
   console.log("server listening on port 8080");
