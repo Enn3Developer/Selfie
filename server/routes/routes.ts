6288ba16cc3feb97e6df2ca8c15b020d683e64c4
Enn3Developer <arturpeshko39@gmail.com>
2025-07-10 19:26:56 +0200

    static site

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -2,8 +2,11 @@ import e from "express";
 import {router as login} from "./login";
 import {router as register} from "./register";
 import {router as user} from "./user/user";
+import express from "express";
 
 export function setupRoutes(app: e.Express) {
+  app.use("/public", express.static("public"));
+
   app.use("/login", login);
   app.use("/register", register);
   app.use("/user", user);
