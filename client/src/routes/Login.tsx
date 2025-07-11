16a5a678f0b6666f597931bb8934800ce21f3bd3
Enn3Developer <arturpeshko39@gmail.com>
2025-07-11 14:28:02 +0200

    error on login/register

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,9 +1,11 @@
 import {EmailInput, PasswordInput} from "../components/TextInput.tsx";
 import {login} from "../net/api.ts";
 import {useNavigate} from "react-router";
+import {useState} from "react";
 
 export function Login() {
   let navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
 
   const loginCall = async (formData: FormData) => {
     let status = await login({
@@ -11,10 +13,13 @@ export function Login() {
       password: formData.get("password")! as string
     });
     if (status === "NO_NET") {
+      setError("No network");
       console.error("No network");
     } else if (status === "NO_USER") {
+      setError("Email or password not valid");
       console.error("Email or password not valid");
     } else {
+      setError(null);
       localStorage.setItem("token", status.token);
       navigate("/");
     }
@@ -22,6 +27,13 @@ export function Login() {
 
   return (
     <>
+      {
+        error != null ?
+          <div role="alert" className="alert alert-error alert-soft">
+            <span>{error}</span>
+          </div>
+          : undefined
+      }
       <form className="flex w-full flex-col p-2 space-y-2 items-center" action={loginCall}>
         <EmailInput name="email" required={true}></EmailInput>
         <PasswordInput name="password" required={true}></PasswordInput>
--- client/src/routes/Register.tsx
+++ client/src/routes/Register.tsx
@@ -3,9 +3,11 @@ import {register} from "../net/api.ts";
 import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
 import AlternateEmailRoundedIcon from '@mui/icons-material/AlternateEmailRounded';
 import {useNavigate} from "react-router";
+import {useState} from "react";
 
 export function Register() {
   let navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
 
   async function registerCall(formData: FormData) {
     let status = await register({
@@ -17,15 +19,19 @@ export function Register() {
     console.log(status);
     switch (status) {
       case "NO_NET":
+        setError("No network");
         console.error("No network");
         break;
       case "EMAIL_FOUND":
+        setError("Email already used");
         console.error("Email already used");
         break;
       case "NO_INSERT":
+        setError("Error on server side, contact website owner");
         console.error("Error on server side");
         break;
       default:
+        setError(null);
         console.log(status.token);
         localStorage.setItem("token", status.token);
         navigate("/");
@@ -34,6 +40,13 @@ export function Register() {
 
   return (
     <>
+      {
+        error != null ?
+          <div role="alert" className="alert alert-error alert-soft">
+            <span>{error}</span>
+          </div>
+          : undefined
+      }
       <form className="flex w-full flex-col p-2 space-y-2 items-center" action={registerCall}>
         <EmailInput name="email" validator={true} required={true} placeholder="mario.rossi@mail.com"/>
         <PasswordInput name="password" validator={true} required={true}/>
